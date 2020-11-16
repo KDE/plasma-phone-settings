788d79e1114fcd01d89ca0a04fd3572dc3a27c3d
Jonah Brüchert <jbb.prv@gmx.de>
2020-11-17 00:26:48 +0100

    Workaround webengine crashes when resizing the web view

--- etc/profile.d/webengine-workaround.sh
+++ etc/profile.d/webengine-workaround.sh
@@ -1,1 +1,4 @@
+# Fixes frames being displayed in wrong order
 export QTWEBENGINE_DISABLE_GPU_THREAD=1
+# Fixes crashes when resizing the web view
+export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu-compositing"
