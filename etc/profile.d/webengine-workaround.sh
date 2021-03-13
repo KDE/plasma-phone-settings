66fc785042b2a2082413a463a3dee85caa0ea4b6
Jonah Brüchert <jbb.prv@gmx.de>
2021-03-13 01:55:11 +0100

    Remove webengine workarounds
    They are not needed any more, and they are bad for performance.

--- etc/profile.d/webengine-workaround.sh
+++
@@ -1,4 +0,0 @@
-# Fixes frames being displayed in wrong order
-export QTWEBENGINE_DISABLE_GPU_THREAD=1
-# Fixes crashes when resizing the web view
-export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu-compositing"
